1fe550a7efcad6db44e286322a4060882adf0cc4
Keith Laurence Esguerra <keithlaurence.esguerra@gmail.com>
2026-01-07 19:40:20 +0800

    moved EmailService, OtpGenerator and OtpService into a dedicated folder within the backend folder. and appropriately renamed the main directory for the backend

---
+++ backend/src/main/java/org/example/labkoto/api/controller/UserController.java
@@ -0,0 +1,49 @@
+package org.example.labkoto.api.controller;
+
+import org.example.labkoto.api.model.User;
+import org.example.labkoto.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping ("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping ("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User savedUser = userService.register(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    //Get user by ID
+    @GetMapping ("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<User> getUser(@PathVariable Integer id) {
+        Optional<User> userOptional = userService.getUser(id);
+        return userOptional.map(ResponseEntity::ok)
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping ("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody User user) {
+        user.setId(id);
+        User updated = userService.updateUser(user);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping ("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
